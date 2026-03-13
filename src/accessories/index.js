b3d893276f4d477d599c522920379f97170cf1c1
Hiddecollee <hiddecollee9@icloud.com>
2026-03-13 17:33:05 +0100

    Update index.js

--- src/accessories/index.js
+++ src/accessories/index.js
@@ -3,7 +3,7 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-export const AccessoriesHandler = require('./accessories.handler').default;
-export const AccessoriesConfig = require('./accessories.config').default;
+export const AccessoriesHandler = require('./accessories.handler');
+export const AccessoriesConfig = require('./accessories.config');
 export const AccessoriesService = require('./accessories.service');
 export const AccessoriesSetup = require('./accessories.setup');
