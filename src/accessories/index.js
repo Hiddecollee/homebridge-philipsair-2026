55aadee716a6a71b246276faca9495c35068e5f0
Hiddecollee <hiddecollee9@icloud.com>
2026-03-13 17:25:39 +0100

    Bump version to 1.0.7 and add createRequire shim
    Bumps package.json version to 1.0.7. Introduces a createRequire(import.meta.url) shim in src/accessories/index.js so CommonJS require can be used from an ES module file, addressing Node 18+ compatibility for accessory imports.

--- src/accessories/index.js
+++ src/accessories/index.js
@@ -1,5 +1,8 @@
 'use-strict';
 
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 export const AccessoriesHandler = require('./accessories.handler').default;
 export const AccessoriesConfig = require('./accessories.config').default;
 export const AccessoriesService = require('./accessories.service');
